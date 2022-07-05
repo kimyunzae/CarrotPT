ba72adb3cfb47021fda26ce03412a6277d349156
kimyunzae <whkim1006@gmail.com>
2022-07-05 23:25:03 +0900

    Chore: frame package creation

---
+++ main/java/com/first/frame/Util.java
@@ -0,0 +1,28 @@
+package com.first.frame;
+
+import java.io.FileOutputStream;
+import org.springframework.web.multipart.MultipartFile;
+
+public class Util {
+	public static void saveFile(MultipartFile mf, String admindir) {
+	
+		byte [] data;
+		String imgname = mf.getOriginalFilename();
+		try {
+			data = mf.getBytes();
+			FileOutputStream fo = 
+					new FileOutputStream(admindir+imgname);
+			fo.write(data);
+			fo.close();
+		
+		}catch(Exception e) {
+			
+		}
+		
+	}
+	
+}
+
+
+
+
