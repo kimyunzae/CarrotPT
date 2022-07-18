64acee1645cd5fe78e6ffc3034ed684f8fc8a654
Moon <103164321+larkmoon@users.noreply.github.com>
2022-07-18 10:08:48 +0900

    Fix. Traienr 접속 시 1page 연결되도록 RequestMapping url 수정

--- main/java/com/first/controller/FTController.java
+++ main/java/com/first/controller/FTController.java
@@ -3,9 +3,9 @@ package com.first.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,30 +19,47 @@ public class FTController {
 
 	@Autowired
 	TrainerBiz biz;
-
-	@RequestMapping("/")
-	public String main(Model m) {
-		
+	
+	@ModelAttribute("totalCount")
+	public int totalCount() {
 		int cnt = 0;
-		try {
-			List<TrainerVO> list = biz.getauthorized();
-			m.addAttribute("trlist", list);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		try {
 			cnt = biz.getcnt();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		m.addAttribute("trcnt", cnt);
+		return cnt;
+	}
+	
+	@ModelAttribute("amount")
+	public int amount() {
+		int amount = 6;
+		return amount;
+	}
+
+
+	@RequestMapping("/")
+	public String main(Model m) {
+		
+		try {
+			List<TrainerVO> list = biz.getbypage(1, 6);
+			m.addAttribute("trlist", list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		m.addAttribute("center", "trainers/trcenter");
 		return "index";
 	}
 	
 	@RequestMapping("/{pageNo}")
 	public String findPage(@PathVariable("pageNo") int pageNo, Model m) {
-		int amount = 5;
+		
+		if(pageNo == 0) {
+			pageNo = 1;
+		}
+		
+		int amount = 6;
 		List<TrainerVO> list = null;
 		try {
 			list = biz.getbypage(pageNo, amount);
@@ -53,32 +70,6 @@ public class FTController {
 		m.addAttribute("trlist", list);
 		return "index";
 	}
-	
-//	@RequestMapping("/{pageNo}")
-//	public String findPage(@PathVariable(value = "pageNo") int pageNo, Model m) {
-//		int pageSize = 6;
-//		Page<TrainerVO> page = biz.findPage(pageNo, pageSize);
-//		List<TrainerVO> list = page.getContent();
-//		m.addAttribute("currentPage", pageNo);
-//		m.addAttribute("totalPages", page.getTotalPages());
-//		m.addAttribute("totalItems", page.getTotalElements());
-//		m.addAttribute("trlist", list);
-	
-//		try {
-//			List<TrainerVO> list = biz.getauthorized();
-//			m.addAttribute("trlist", list);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		try {
-//			cnt = biz.getcnt();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		m.addAttribute("trcnt", cnt);
-//		m.addAttribute("center", "trainers/trcenter");
-//		return "index";
-//	}
 	
 	@RequestMapping("/detail")
 	public String detail(Model m, String id) {
