b1aa3abd6cd1f55cb34413d288c83aff5e1405ce
kimyunzae <whkim1006@gmail.com>
2022-07-20 16:42:26 +0900

    Chore: adding update features

--- main/java/com/first/controller/MypageController.java
+++ main/java/com/first/controller/MypageController.java
@@ -54,17 +54,17 @@ public class MypageController {
 			e.printStackTrace();
 		}
 	
-		m.addAttribute("trainercenter","mypage/trprofile");
+		m.addAttribute("trainercenter","mypage/traccount");
 		return "index";
 	}
 	
 	//트레이너 마이페이지: 경력 및 수상
-	@RequestMapping("/trcareer")
-	public String trcareer(Model m, HttpSession session) {
+	@RequestMapping("/trprofile")
+	public String trprofile(Model m, HttpSession session) {
 		try {
 			TrainerVO trainer = (TrainerVO) session.getAttribute("logincust");
 			m.addAttribute("center", "mypage/trmypage");
-			m.addAttribute("trainercenter","mypage/trcareer");
+			m.addAttribute("trainercenter","mypage/trprofile");
 			m.addAttribute("trainer", trainer);
 
 		} catch (Exception e) {
