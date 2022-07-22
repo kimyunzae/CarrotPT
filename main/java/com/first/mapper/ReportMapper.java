dc7f4d712a9c901dcfba8f22f0ae79117e0f2eca
Moon <103164321+larkmoon@users.noreply.github.com>
2022-07-22 17:47:02 +0900

    Feat. 관리자페이지 뱃지 알림-대상 개수가 0 이상일 때만 표시

--- main/java/com/first/mapper/ReportMapper.java
+++ main/java/com/first/mapper/ReportMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import com.first.vo.NewAlarmVO;
 import com.first.vo.ReportVO;
 
 @Repository
