1d2caa8cf1cfce6c565175441dce3181508fcc1f
Moon <103164321+larkmoon@users.noreply.github.com>
2022-07-22 13:52:45 +0900

    Feat. 관리자페이지 트레이너 상태 '거절'로 변경 시 select로 사유 선택

--- main/java/com/first/controller/AJAXController.java
+++ main/java/com/first/controller/AJAXController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.first.biz.TraineeBiz;
