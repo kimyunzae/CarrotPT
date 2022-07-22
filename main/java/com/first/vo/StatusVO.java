d3358c6f32efae9178b6033607cb361889d4ba37
Moon <103164321+larkmoon@users.noreply.github.com>
2022-07-22 14:47:25 +0900

    Fix: 지도로 찾기 검색어 수정 - input 요소 value 붙이기

--- main/java/com/first/vo/StatusVO.java
+++ main/java/com/first/vo/StatusVO.java
@@ -3,7 +3,6 @@ package com.first.vo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
