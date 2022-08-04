5696607ea66d40bcf02f2d995697ad1d1b488f5d
Moon <103164321+larkmoon@users.noreply.github.com>
2022-08-04 14:34:41 +0900

    Feat. 매칭 페이지 현재 시각 표시

--- main/java/com/first/controller/MatchingController.java
+++ main/java/com/first/controller/MatchingController.java
@@ -1,8 +1,13 @@
 package com.first.controller;
 
+import java.time.LocalDateTime;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MatchingController {
@@ -11,5 +16,32 @@ public class MatchingController {
 	public String matching(Model m) {
 //		m.addAttribute("center", "matching/matching");
 		return "matching/matching";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/getTime")
+	public String gettime() {
+		JSONArray currentArr = new JSONArray();
+		JSONObject currentObj = new JSONObject();
+		LocalDateTime now = LocalDateTime.now();
+		
+		int year = now.getYear();
+		int month = now.getMonthValue();
+		int date = now.getDayOfMonth();
+		int day = now.getDayOfWeek().getValue();
+		int hour = now.getHour();
+		int minute = now.getMinute();
+		int second = now.getSecond();
+		
+		currentObj.put("year", year);
+		currentObj.put("month", month);
+		currentObj.put("date", date);
+		currentObj.put("day", day);
+		currentObj.put("hour", hour);
+		currentObj.put("minute", minute);
+		currentObj.put("second", second);
+		
+		currentArr.put(currentObj);
+		return currentArr.toString();
 	}
 }
