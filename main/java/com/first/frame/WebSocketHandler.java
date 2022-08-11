64f01c037d122435c90aaa0d79d39b41e2ab8558
Moon <103164321+larkmoon@users.noreply.github.com>
2022-08-11 11:03:25 +0900

    Test. ChannelInterceptor Test

--- main/java/com/first/frame/WebSocketHandler.java
+++ main/java/com/first/frame/WebSocketHandler.java
@@ -1,20 +1,25 @@
 package com.first.frame;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class WebSocketHandler implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         if (StompCommand.SUBSCRIBE == accessor.getCommand()) {
-            System.out.println("New connection");
+            System.out.println("New STOMP connection");
+        }else if(StompCommand.DISCONNECT == accessor.getCommand()) {
+        	System.out.println("STOMP Disconnected");
         }
 
         return message;
